ffffbe798f4bfb4cc37f3ef880766026f4dd1ad5
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-01-28 11:43:26 -0500

    install django / create object menu / add pizza

---
+++ pizzamama/menu/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    ingredients = models.CharField(max_length=200)
+    price = models.FloatField(default = 0)
+    vegetarian = models.BooleanField(default = False)
+
+
+
+# Create your models here.
