743b46833340d3dc6dc5c3a84e2d8cde95f80760
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-01-29 11:46:21 -0500

    menu ok / creating main page

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -8,5 +8,5 @@ def index(request):
     pizzas_names_and_price = [pizza.name + ":" + str(pizza.price) + "$" for pizza in pizzas]
     pizzas_names_and_price_str = ", ".join(pizzas_names_and_price)
     return HttpResponse("Les pizzas: " + pizzas_names_and_price_str)"""
-   pizzas = Pizza.objects.all()
+   pizzas = Pizza.objects.all().order_by('price')
    return render(request, 'menu/index.html',{'pizzas': pizzas})
